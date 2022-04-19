d1b9e5968364f40d8c3d06de2989c8e25018a9d0
jacob nordan <jacob.nordan113322@gmail.com>
2022-04-19 12:05:31 -0400

    added switch case to add in random characters from each dif type of character can be included

--- main.cpp
+++ main.cpp
@@ -29,4 +29,73 @@ void generate_password(int length)
     string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string s_symbol = "!@#$%&";
     string number = "0123456789";
+
+    // initializing local variables
+    int key, count_alphabet = 0, count_ALPHABET = 0, count_number = 0, count_s_symbol = 0;
+
+    // Count will store the length of the password being created,
+    // initially this will be zero(0)
+    int count = 0;
+    while (count < length)
+    {
+
+        int k = selectArray();
+
+        if (count == 0)
+        {
+            k = k % 3;
+            if (k == 0)
+                k++;
+        }
+        switch (k)
+        {
+        case 1:
+            if ((count_alphabet == 2) && (count_number == 0 || count_ALPHABET == 0 || count_ALPHABET == 1 || count_s_symbol == 0))
+                break;
+
+            key = getKey();
+            password = password + alphabet[key];
+            count_alphabet++;
+            count++;
+            break;
+
+        case 2:
+            if ((count_ALPHABET == 2) && (count_number == 0 || count_alphabet == 0 || count_alphabet == 1 || count_s_symbol == 0))
+                break;
+            key = getKey();
+            password = password + ALPHABET[key];
+            count_ALPHABET++;
+            count++;
+            break;
+
+        case 3:
+            if ((count_number == 1) && (count_alphabet == 0 || count_alphabet == 1 || count_ALPHABET == 1 || count_ALPHABET == 0 || count_s_symbol == 0))
+                break;
+
+            key = getKey();
+            key = key % 10;
+            password = password + number[key];
+            count_number++;
+            count++;
+            break;
+
+        case 4:
+            if ((count_s_symbol == 1) && (count_alphabet == 0 || count_alphabet == 1 || count_ALPHABET == 0 || count_ALPHABET == 1 || count_number == 0))
+                break;
+
+            key = getKey();
+            key = key % 6;
+            password = password + s_symbol[key];
+            count_s_symbol++;
+            count++;
+            break;
+        }
+    }
+
+    cout << "\n-----------------------------\n";
+    cout << "         PASSWORD             \n";
+    cout << "------------------------------\n\n";
+    cout << " " << password;
+    cout << "\n\nPress any key continue \n";
+    getchar();
 }
